6fa2be106747c1c5b9af38fb374f5036fbbfb663
ewhd <ewhd22@gmail.com>
2021-03-05 00:54:45 -0800

    challenge304 finished

--- challenge304-menu-conditionals.sh
+++ challenge304-menu-conditionals.sh
@@ -17,19 +17,31 @@ input_var=""
 option_selector() {
     if [[ $input_var == 1 ]]; then
         echo "Hello World"
-    if [[ $input_var == 2 ]]; then
-        ping 127.0.0.1
-    if [[ $input_var == 3 ]]; then
+    elif [[ $input_var == 2 ]]; then
+        ping -c 4 127.0.0.1
+    elif [[ $input_var == 3 ]]; then
         ip a
-    if [[ $input_var == 4 ]]; then
+    elif [[ $input_var == 4 ]]; then
         exit
+    else
+        echo "
+            Invalid input. PLease try again.
+            "
+    fi
 }
 
 # Menu
 
 while true; do
-    echo "option menu"
-    read input_var
+    echo "
+    Please select from the following optinos:
+        1. Hello world (prints “Hello world!” to the screen)
+        2. Ping self (pings this computer’s loopback address)
+        3. IP info (print the network adapter information for this computer)
+        4. Exit
+
+    "
+    read -p "Input option (1/2/3/4): " input_var
     option_selector
 done
 
