49ea2eb9dda26e20e6b8332df18a7f20a2b49b3c
ewhd <ewhd22@gmail.com>
2021-03-05 22:17:31 -0800

    challenge305 started

---
+++ challenge305-clear-logs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script:                       challenge305-clear-logs.sh
+# Author:                       Ethan Denny
+# Date of latest revision:      3/5/2021
+# Purpose:     
+
+
+
+# Define Variables
+
+
+# Define Functions
+
+
+# Main
+
+
+
+
+# End
