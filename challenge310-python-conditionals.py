a26b18422f73fb82058c0708539135def70f1831
Ethan Denny <ewhd22@gmail.com>
2021-03-12 22:31:13 -0800

    Create challenge310-python-conditionals.py

---
+++ challenge310-python-conditionals.py
@@ -0,0 +1,6 @@
+#!/user/bin/env python3
+
+# Script:                       challenge310-python-conditionals.py
+# Author:                       Ethan Denny
+# Date of latest revision:      3/12/2021
+# Purpose:     Demonstrate the use of various conditional statements
