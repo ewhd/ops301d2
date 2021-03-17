ecdd9c9474f5ac26aeecebdf4c4b76f496601784
Ethan Denny <ewhd22@gmail.com>
2021-03-16 21:49:50 -0700

    Update challenge310-python-conditionals.py

--- challenge310-python-conditionals.py
+++ challenge310-python-conditionals.py
@@ -2,7 +2,7 @@
 
 # Script:                       challenge310-python-conditionals.py
 # Author:                       Ethan Denny
-# Date of latest revision:      3/12/2021
+# Date of latest revision:      3/14/2021
 # Purpose:     Demonstrate the use of various conditional statements
 
 
